5285d5de46f680da7228487800eaf96e399ecd4f
Huertas <Joshua_Huertas1@Baylor.edu>
2018-11-12 18:58:53 -0600

    Joshua Huertas attempted to make a square move (with gravity)

--- main.cpp
+++ main.cpp
@@ -14,44 +14,65 @@ void plotImage(SDL_Plotter &game, int i, int j, int r, int g, int b)
 	}
 }
 
-int main(int argc, char ** argv)
-{
-	SDL_Plotter g(768,1024);
+int main(int argc, char ** argv) {
+	SDL_Plotter g(768, 1024);
 	bool stopped = false;
-	bool colored = false;
-	int x,y, xd, yd;
-	int R,G,B;
+//	bool colored = false;
+//	int x, y, xd, yd;
+	int playerx = 50, playery = 0, playerWidth = 10, playerHeight = 10, jumptime = -9;
+//	int R, G, B;
 
-	plotImage(g,1,1,0,0,0);
-	plotImage(g,1,710,135,45,45);
+//	plotImage(g, 0, 0, 0, 0, 0);
+//	plotImage(g, 0, 710, 135, 45, 45);
 
 	while (!g.getQuit())
 	{
-		if (!stopped)
+		//Player input
+		if (g.getKey() == 'P')
 		{
-
+			stopped = !stopped;
+		}
+		else if (g.getKey() == RIGHT_ARROW)
+		{
+			playerx = min(playerx + 1, 1024 - playerWidth);
+		}
+		else if (g.getKey() == LEFT_ARROW)
+		{
+			playerx = max(playerx - 1, 0);
+		}
+		else if (g.getKey() == UP_ARROW)
+		{
+			if(jumptime == -9)
+				jumptime = 10;
 		}
 
-		if (g.kbhit())
+		if(jumptime > -10)
 		{
-			if (g.getKey() == 'P')
+			jumptime--;
+
+			if(jumptime > 0)
+				playery = max(playery - 1, 0);
+			else
+				playery = min(playery + 1, 768 - playerHeight);
+		}
+
+		//Draw loop
+
+		g.clear();
+
+		for (int col = playerx; col < playerWidth + playerx; col++)
+		{
+			for (int row = playery; row < playerHeight + playery; row++)
 			{
-				stopped = !stopped;
-			}
-			else if (g.getKey() == RIGHT_ARROW)
-			{
-				// Move mario right
-			}
-			else if (g.getKey() == LEFT_ARROW)
-			{
-				// Move mario left
-			}
-			else if (g.getKey() == UP_ARROW)
-			{
-				// Mario jump
+				g.plotPixel(col, row, 255, 0, 0);
 			}
 		}
 
 		g.update();
+
+		if (g.kbhit())
+		{
+			g.getKey();
+		}
 	}
 }
